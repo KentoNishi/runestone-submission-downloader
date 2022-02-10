27e40d2abb77ef6efe98057c79fe7d89c8870d26
Kento Nishi <kento24gs@outlook.com>
2022-02-10 14:27:50 -0800

    hey it kinda works!

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,12 +1,12 @@
 import JSZip from 'jszip';
-
-console.log(JSZip());
+import { saveAs } from 'file-saver';
 
 const questionsElement = Array.from(document.querySelectorAll('h3'))
   .filter(e => e.textContent === 'Questions')[0]
   .parentElement;
 
-const sendRequests = () => {
+const sendRequests = async () => {
+  const zip = JSZip();
   const problems = (
     Array.from(document.querySelectorAll('#questionselector option')) as HTMLOptionElement[]
   ).map(e => e.value);
@@ -14,20 +14,26 @@ const sendRequests = () => {
     Array.from(document.querySelectorAll('#studentselector option')) as HTMLOptionElement[]
   ).map(e => e.value);
   console.log(problems, students);
+  const promises = [];
   problems.forEach(problem => {
-    students.forEach(async student => {
-      console.log(await (await fetch('https://runestone.academy/ns/assessment/gethist', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          acid: problem,
-          sid: student,
-        }),
-      })).json() as HistoryResponse);
+    students.forEach(student => {
+      promises.push((async () => {
+        const data = await (await fetch('https://runestone.academy/ns/assessment/gethist', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            acid: problem,
+            sid: student,
+          }),
+        })).json() as HistoryResponse;
+        zip.file(`${problem}/${student}.py`, data.detail.history.pop());
+      })());
     });
   });
+  await Promise.allSettled(promises);
+  saveAs(await zip.generateAsync({ type: 'blob' }), 'submissions.zip');
 };
 
 const createButton = () => {
@@ -35,7 +41,7 @@ const createButton = () => {
   const b = document.createElement('button');
   b.className = 'btn btn-primary';
   b.id = 'custom-button';
-  b.innerText = 'Download All Student Submissions';
+  b.innerText = 'Download All Student Submissions for Selected Problems';
   b.style.setProperty('background-color', '#0f1b84', 'important');
   b.style.display = 'block';
   questionsElement.appendChild(b);
